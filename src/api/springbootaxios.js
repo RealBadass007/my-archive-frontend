be3bf1926d79e98a55400e6f51b312765dfd14f2
Chirag <chiragbangera007@gmail.com>
2024-01-12 00:23:21 +0530

    adding springbootaxios.js to .gitignore file

--- src/api/springbootaxios.js
+++ src/api/springbootaxios.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
+//ignoring this file in .gitignore
 const arr = window.location.href.split(":");
 console.log("Hello from springboot axios url => " + arr[0] + ":" + arr[1]);
 
 export default axios.create({
-    baseURL: arr[0] + ":" + arr[1] + "api"
+    baseURL: arr[0] + ":" + arr[1] + "  api"
 });
